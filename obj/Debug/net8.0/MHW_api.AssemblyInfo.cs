94f285f4e6aae38340afa2b4d384a516b56d95cc
Bruno <b.rezzio.93@gmail.com>
2025-04-05 20:47:04 -0300

    actualización gitignore

--- obj/Debug/net8.0/MHW_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/MHW_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHW_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86d613c8b5b0bde03f9f60b3e8c0230c423f835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6337c923700562bc1d923a770ae0bb7cbb324edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHW_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHW_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
